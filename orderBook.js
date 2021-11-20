8e8ab0dfbe324dd9c7f45ea1657401ff84363578
Yl9464 <86930915+Yl9464@users.noreply.github.com>
2021-11-20 14:19:34 -0500

    set up lets for type quantity and price

--- orderBook.js
+++ orderBook.js
@@ -1,22 +1,26 @@
 
 
 const reconcileOrder = (existingBook, incomingOrder) => {
+  if (existingBook.length < 1) existingBook.push(incomingOrder)
+
   let type = ['sell', 'buy']
-  let updatedBook = [existingBook, incomingOrder]
+  let quantity = [5, 10, 12, 15, 25]
+  let price = [5900, 5950, 6000, 6150, 6250, 6950]
+  const updatedBook = [existingBook, incomingOrder]
 
-  if (existingBook.length < 1) {
-    incomingOrder.push(existingBook)
-  }
-  if (incomingOrder === type) {
+  if (incomingOrder.type === type[0]) {
     type = 'sell'
-    updatedBook.push(incomingOrder)
   }
 
-  else {
-    return updatedBook
+  if (incomingOrder.quantity === quantity[1]) {
+    quantity = 10
   }
+
+  if (incomingOrder.price === price[3]) {
+    price = 6150
+  }
+
+  return updatedBook
 }
-
-
 
 module.exports = reconcileOrder
