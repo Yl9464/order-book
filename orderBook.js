35fbe328268c696950c8f7abbccb8351bcc37df9
Yl9464 <86930915+Yl9464@users.noreply.github.com>
2021-11-30 16:23:24 -0500

    returns objects for first test

--- orderBook.js
+++ orderBook.js
@@ -1,25 +1,13 @@
+const reconcileOrder = (existingBook, incomingOrder, updatedBook) => {
+  const orderOne = firstOrder(existingBook, incomingOrder, updatedBook)
 
-// existingBook = ['type', 'quantity', 'price']
-// incomingOrder =  ['type', 'quantity', 'price']
-
-// type= ['sell', 'buy']
-// quantity= [5, 10, 12, 15, 25]
-// price = [5900, 5950, 6000, 6150, 6250, 6950]
-
-// updatedBook === ['existingBook', 'incomingOrder']
-// return the updatedBook
-
-
-const reconcileOrder = (existingBook) => {
-// let existingBook = ['type', 'quantity', 'price']
-  let updatedBook = []
-
-  for (let i = 0; i < existingBook.length; i++) {
-    if (updatedBook.indexOf(existingBook[1] < 0)) updatedBook.push(existingBook[i])
-  }
-
-  return updatedBook
+  console.log(orderOne)
 }
 
+const firstOrder = (existingBook, incomingOrder, updatedBook) => {
+  const orderOne = { ...incomingOrder }
+  console.log(orderOne)
+
+}
 
 module.exports = reconcileOrder
