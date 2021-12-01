99d7e65620e4542711d4b810b5d38240c488588d
Yl9464 <86930915+Yl9464@users.noreply.github.com>
2021-11-30 22:23:18 -0500

    all tests passing

--- orderBook.js
+++ orderBook.js
@@ -1,17 +1,44 @@
-const reconcileOrder = (existingBook, incomingOrder, updatedBook) => {
-  const startBook = startingOrder(existingBook, incomingOrder, updatedBook)
-
-  console.log(startBook)
-}
-
-const startingOrder = (existingBook, incomingOrder, updatedBook) => {
+const reconcileOrder = (existingBook, incomingOrder) => {
   const firstOrder = { ...incomingOrder }
 
   if (existingBook.length === 0) {
-    return updatedBook = firstOrder
+    existingBook.push(firstOrder)
+
+    return existingBook
+  }
+  // looping over book
+  let updatedBook = []
+  let notUpdated = []
+
+  for (let i = 0; i < existingBook.length; i++) {
+    let existingOrder = existingBook[i]
+
+    if (dealOrNoDeal(existingOrder, incomingOrder)) {
+      const lowestNum = Math.min(existingOrder.quantity, incomingOrder.quantity)
+
+      existingOrder.quantity = existingOrder.quantity - lowestNum
+      incomingOrder.quantity = incomingOrder.quantity - lowestNum
+
+
+      if (existingOrder.quantity > 0) updatedBook.push(existingOrder)
+    }
+    else {
+      notUpdated.push(existingOrder)
+    }
   }
 
-  return updatedBook
+  if (incomingOrder.quantity > 0) updatedBook.push(incomingOrder)
+
+  return [...notUpdated, ...updatedBook]
+}
+
+//extra credit if statements in here
+const dealOrNoDeal = (existingOrder, incomingOrder) => {
+  if (existingOrder.type !== incomingOrder.type && existingOrder.price === incomingOrder.price) {
+    return true
+  }
+
+  return false
 }
 
 module.exports = reconcileOrder
